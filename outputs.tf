072855e23e36a3f706a027df072c2137f7264811
Chris Gilmer <chris.gilmer@gmail.com>
2021-03-12 10:40:01 -0800

    Fix outputs when roles are not enabled. Add max session duration as output.

--- outputs.tf
+++ outputs.tf
@@ -6,20 +6,25 @@ output "identity_provider_arn" {
 
 output "role_arn_admin" {
   description = "ARN for Admin role"
-  value       = aws_iam_role.admin_role[0].arn
+  value       = var.enable_admin_role ? aws_iam_role.admin_role[0].arn : null
 }
 
 output "role_arn_power_user" {
   description = "ARN for Power User role"
-  value       = aws_iam_role.power_user_role[0].arn
+  value       = var.enable_power_user_role ? aws_iam_role.power_user_role[0].arn : null
 }
 
 output "role_arn_read_only" {
   description = "ARN for Read Only role"
-  value       = aws_iam_role.read_only_role[0].arn
+  value       = var.enable_read_only_role ? aws_iam_role.read_only_role[0].arn : null
 }
 
 output "assume_role_policy" {
   value       = data.aws_iam_policy_document.role_assume.json
   description = "IAM Assume Role Policy to use with other SAML enabled roles."
+}
+
+output "max_session_duration_seconds" {
+  value       = var.max_session_duration_seconds
+  description = "Maximum CLI Session duration in seconds"
 }
